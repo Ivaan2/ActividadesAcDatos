4f92dbdcc30683c432daee708d062a880ae57194
Ivaan2 <i.morales.mellado@iescristobaldemonroy.es>
2021-10-18 13:47:17 +0200

    hay que separar

--- src/Pruebas/EjercicioConcesionario.java
+++ src/Pruebas/EjercicioConcesionario.java
@@ -1,20 +1,16 @@
 package Pruebas;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Collections;
 
-public class EjercicioConcesionario {
+public class EjercicioConcesionario implements Comparable<EjercicioConcesionario>{
     String marca, modelo;
-    Date fMatriculacion;
+    LocalDate fMatriculacion;
     int kms, precio;
 
-    public EjercicioConcesionario(String marca, String modelo, Date fMatriculacion, int kms, int precio){
+    public EjercicioConcesionario(String marca, String modelo, LocalDate fMatriculacion, int kms, int precio){
         this.marca = marca;
         this.modelo = modelo;
         this.fMatriculacion = fMatriculacion;
@@ -34,29 +30,58 @@ public class EjercicioConcesionario {
     }
 
     public static void main(String[] args) {
-        String garaje = "src/Archivos/coches.txt";
-        leerAtributos(garaje);
+        String destinoCSV = "src/Archivos/coches.txt";
+        String garaje = "src/Archivos/informacion.txt";
+        leerAtributos(garaje, destinoCSV);
     }
 
-    public static void leerAtributos(String garaje){
+    public static void leerAtributos(String garaje, String destinoCSV){
         String linea = null;
-        EjercicioConcesionario coche;
+        EjercicioConcesionario coche = null;
+        BufferedWriter bw = null;
+        ArrayList<EjercicioConcesionario> conjuntoCoches = new ArrayList<EjercicioConcesionario>();
+
         try(BufferedReader br = new BufferedReader(new FileReader(garaje));){
             {
+                bw = new BufferedWriter(new FileWriter(destinoCSV));
                 String[] atributos;
                 while((linea = br.readLine()) != null){
                     atributos = linea.split(";");
                     String dia = atributos[2].substring(0,2);
                     String mes = atributos[2].substring(2,4);
                     String anno = atributos[2].substring(4,8);
-                    Date date = new Date(Integer.valueOf(dia), Integer.valueOf(mes), Integer.valueOf(anno));
+                    LocalDate date = LocalDate.of(Integer.valueOf(anno), Integer.valueOf(mes), Integer.valueOf(dia));
+
 
                     coche = new EjercicioConcesionario(atributos[0], atributos[1], date, Integer.parseInt(atributos[3]), Integer.parseInt(atributos[4]));
-                    System.out.println(coche);
+                    conjuntoCoches.add(coche);
+
+
+                   // System.out.println(coche);
                 }
+                Collections.sort(conjuntoCoches);
+                for(EjercicioConcesionario coches : conjuntoCoches){
+                    System.out.println(coches.toString());
+                    bw.write(coches.toString());
+                    bw.newLine();
+                }
+
             }
         }catch (IOException e){
             e.printStackTrace();
+        }finally {
+            if (bw != null){
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+
+    @Override
+    public int compareTo(EjercicioConcesionario o) {
+        return this.kms - o.kms;
     }
 }
